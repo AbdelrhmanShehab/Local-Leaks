4b598a6d7a496250240962aff805d633668d6297
MGElnaggar <Mostafagamal5827@gmail.com>
2024-11-18 21:25:13 +0200

    commit

--- src/Pages/Home/HomeSections/OurServices/OurServices.jsx
+++ src/Pages/Home/HomeSections/OurServices/OurServices.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import style from './OurServices.module.css';
+import ourServicesImg from '../../../../Assets/OurServices.png'
 
 const OurServices = () => {
     return (
         <div className={style.ourServices}>
-        
+            <div className={style.ourServicesContainer}>
+                <div className={style.servicesDescription}>
+                    <p>
+                        <strong>Exclusive Local Collections; </strong>
+                        Shop unique and exclusive fashion from the best local brands, all in one place.
+                    </p>
+                    <p>
+                        <strong>Custom Modeling; </strong>
+                        Choose a model that matches your body type to see how clothes fit before you buy.
+                    </p>
+                    <p>
+                        <strong>Personalized Recommendations; </strong>
+                        Get tailored style suggestions based on your preferences and trending local brands.
+                    </p>
+                </div>
+                <div className={style.servicesImage}>
+                    <h1>Our Services</h1>
+                    <img src={ourServicesImg} alt="Our Services" />
+                    <div className={style.imageCircle}></div>
+                </div>
+            </div>
         </div>
     );
 };
